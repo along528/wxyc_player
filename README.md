7617ad88b7a61d4efa47877820337c68ca9cd34d
dvd <david@chartbeat.com>
2015-12-13 00:31:48 -0500

    update colors

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ To Run:
 
 
 All credit to [Alex Long](https://github.com/along528) with some mods by [dvd](https://github.com/dvdokkum)
+
+todo
+
+- switch from parsing html playlist to using json feed: http://wxyc.info/playlists/recentEntries?n=20
--- wxyc.py
+++ wxyc.py
@@ -63,14 +63,14 @@ def sortplaylist(playlist):
 def initcurses(curses):
 	#setup curses module for refreshing terminal
 	curses.start_color()
-	curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+	curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
 	curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
-	curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-	curses.init_pair(4, curses.COLOR_RED, curses.COLOR_BLACK)
-	curses.init_pair(5, curses.COLOR_GREEN, curses.COLOR_WHITE)
+	curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
+	curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+	curses.init_pair(5, curses.COLOR_BLACK, curses.COLOR_WHITE)
 	curses.init_pair(6, curses.COLOR_BLACK, curses.COLOR_WHITE)
-	curses.init_pair(7, curses.COLOR_YELLOW, curses.COLOR_WHITE)
-	curses.init_pair(8, curses.COLOR_RED, curses.COLOR_WHITE)
+	curses.init_pair(7, curses.COLOR_BLACK, curses.COLOR_WHITE)
+	curses.init_pair(8, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
 def updateplaylist():
 
